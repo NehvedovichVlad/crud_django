30a1cc474064f2f19d313797dec1a896cbfc4adf
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2020-11-13 15:48:55 +0300

    show admin  panel

--- accounts/models.py
+++ accounts/models.py
@@ -8,3 +8,12 @@ class Person(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     created_info = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     update_info = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Персона'
+        verbose_name_plural = 'Персоны'
+        ordering = ['-created_info']
