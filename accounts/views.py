23c2ce8317eef5534bd00150a5df7bea820d812f
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2020-11-14 19:21:21 +0300

    edit person

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from django.urls import reverse_lazy
@@ -21,9 +21,24 @@ class CreatePerson(CreateView):
 
 
 class DeletePerson(DeleteView):
-    template_name = 'accounts/add_person.html'
+    template_name = 'accounts/person.html'
     success_url = reverse_lazy('home')
 
     def get_object(self):
         id_ = self.kwargs.get("id")
         return get_object_or_404(Person, id=id_)
+
+
+def edit_person(request, id):
+
+    person = Person.objects.get(id=id)
+    form = PersonForm(instance=person)
+
+    if request.method == 'POST':
+        form = PersonForm(request.POST, instance=person)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'accounts/edit_person.html', context)
