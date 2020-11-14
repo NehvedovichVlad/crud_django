927dc4ec3742bc8200318f5c889a57a0e3d94412
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2020-11-14 12:36:05 +0300

    delete object

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ from .views import *
 urlpatterns = [
     path('', person, name='home'),
     path('add_person/', CreatePerson.as_view(), name='add_person'),
+    path('<int:id>/delete/', DeletePerson.as_view(), name='delete_person'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from accounts.forms import PersonForm
 from accounts.models import Person
@@ -18,3 +18,12 @@ class CreatePerson(CreateView):
     form_class = PersonForm
     template_name = 'accounts/add_person.html'
     success_url = reverse_lazy('home')
+
+
+class DeletePerson(DeleteView):
+    template_name = 'accounts/add_person.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Person, id=id_)
